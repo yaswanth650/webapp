efcab015c0fd43e8ba7554fce1d3bf3f78cf2a60
yaswanth650 <104341482+yaswanth650@users.noreply.github.com>
2022-09-21 10:06:07 +0530

    Update owasp-dependency-check.sh

--- owasp-dependency-check.sh
+++ owasp-dependency-check.sh
@@ -21,7 +21,7 @@ docker pull owasp/dependency-check:$DC_VERSION
 docker run --rm \
     -e user=$USER \
     -u $(id -u ${USER}):$(id -g ${USER}) \
-    --volume $(pwd):/src:z \
+    --volume $(pwd):/z \
     --volume "$DATA_DIRECTORY":/usr/share/dependency-check/data:z \
     --volume $(pwd)/odc-reports:/report:z \
     owasp/dependency-check:$DC_VERSION \
