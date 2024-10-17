a60e411299dfd3a261a2e84fe284b4ffb0440aec
yaswanth650 <104341482+yaswanth650@users.noreply.github.com>
2024-10-17 23:02:50 +0530

    Update test.rb

--- test.rb
+++ test.rb
@@ -6,3 +6,27 @@ describe port(443) do
   it { should be_listening }
   its('protocols') {should include 'tcp'}
 end
+
+ describe file("/trufflehog") do
+     it { should exist }
+  end
+
+describe file('/trufflehog') do
+     its('size') { should be >= 120 }
+ end
+
+describe sshd_config do
+     its('Protocol') { should eq '2' }
+   end
+
+describe sshd_config do
+     its('Protocol') { should eq 2 }
+ end
+ describe sshd_config do
+     its('Ciphers') { should_not match '/cbc/' }
+   end
+
+ describe docker_image(name: 'gasellix/trufflehog') do
+  it { should exist }
+  it { should be_running }
+end
